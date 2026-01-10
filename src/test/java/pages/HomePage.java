6ccd1a970e0290693e53944c62d6fa324348e455
GeekIronic <juanpablomahechap@gmail.com>
2026-01-10 15:46:42 -0500

    Se agrega el page Home con la ubicación de los campos a validar y el definitions, modificando el ultimo paso para validar el carrito de compra y su redireccion

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,16 +4,20 @@ public class HomePage {
 
     private String TxtTiteHome = "//div[@class='app_logo']";
     private String TxtNavarHome = "//div[@id='header_container']//child::div[@class='primary_header']";
-    private String IconMenu = "";
+    private String IconMenu = "//button[@id='react-burger-menu-btn']";
     private String IconCar = "//a[@class='shopping_cart_link']";
+    private String BmMenu = "//div[@class='bm-menu']";
+    private String TxtListMenu = "//nav[@class='bm-item-list']//child::a[@class='bm-item menu-item']";
 
     public HomePage() {}
 
-    public HomePage(String txtTiteHome, String txtNavarHome, String iconMenu, String iconCar) {
+    public HomePage(String txtTiteHome, String txtNavarHome, String iconMenu, String iconCar, String bmMenu, String txtListMenu) {
         TxtTiteHome = txtTiteHome;
         TxtNavarHome = txtNavarHome;
         IconMenu = iconMenu;
         IconCar = iconCar;
+        BmMenu = bmMenu;
+        TxtListMenu = txtListMenu;
     }
 
     public String getTxtTiteHome() {
@@ -46,5 +50,21 @@ public class HomePage {
 
     public void setIconCar(String iconCar) {
         IconCar = iconCar;
+    }
+
+    public String getBmMenu() {
+        return BmMenu;
+    }
+
+    public void setBmMenu(String bmMenu) {
+        BmMenu = bmMenu;
+    }
+
+    public String getTxtListMenu() {
+        return TxtListMenu;
+    }
+
+    public void setTxtListMenu(String txtListMenu) {
+        TxtListMenu = txtListMenu;
     }
 }
