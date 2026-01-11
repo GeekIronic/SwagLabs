2bed8320b6291d8b960ef3178416d3a5cf5448cd
GeekIronic <juanpablomahechap@gmail.com>
2026-01-11 15:19:00 -0500

    Se agrega el drivermanager e firefox y el codigo de webdrivermanager que administra el driver de firefox

--- src/test/java/hooks/WebDriverManager.java
+++ src/test/java/hooks/WebDriverManager.java
@@ -5,6 +5,8 @@ package hooks;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -13,33 +15,42 @@ import java.util.Map;
 public class WebDriverManager {
 
     public static WebDriver driver;
+    public static final String BROWSER = "chrome";
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
 
-            // ... dentro de getDriver()
-            ChromeOptions options = new ChromeOptions();
+            if (BROWSER.equals("chrome")) {
+                System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
+                // ... dentro de getDriver()
+                ChromeOptions options = new ChromeOptions();
+                // ARGUMENTOS DE INICIO
+                options.addArguments("--start-maximized");
+                options.addArguments("--disable-blink-features=AutomationControlled");
+                options.addArguments("--disable-save-password-bubble");
+                options.addArguments("--disable-autofill-keyboard-accessor-stack");
+                options.addArguments("--guest"); // <--- FUERZA PERFIL LIMPIO
+                // PREFERENCIAS (Nivel profundo)
+                Map<String, Object> prefs = new HashMap<>();
+                prefs.put("credentials_enable_service", false);
+                prefs.put("profile.password_manager_enabled", false);
+                prefs.put("credentials_enable_autosignin", false); // <--- EVITA EL POPUP DE SMART LOCK
+                options.setExperimentalOption("prefs", prefs);
+                // EXCLUSIÓN DE BANDERAS
+                options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
+                options.setExperimentalOption("useAutomationExtension", false);
 
-// ARGUMENTOS DE INICIO
-            options.addArguments("--start-maximized");
-            options.addArguments("--disable-blink-features=AutomationControlled");
-            options.addArguments("--disable-save-password-bubble");
-            options.addArguments("--disable-autofill-keyboard-accessor-stack");
-            options.addArguments("--guest"); // <--- FUERZA PERFIL LIMPIO
-
-// PREFERENCIAS (Nivel profundo)
-            Map<String, Object> prefs = new HashMap<>();
-            prefs.put("credentials_enable_service", false);
-            prefs.put("profile.password_manager_enabled", false);
-            prefs.put("credentials_enable_autosignin", false); // <--- EVITA EL POPUP DE SMART LOCK
-            options.setExperimentalOption("prefs", prefs);
-
-// EXCLUSIÓN DE BANDERAS
-            options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
-            options.setExperimentalOption("useAutomationExtension", false);
-
-            driver = new ChromeDriver(options);
+                driver = new ChromeDriver(options);                
+            } else if (BROWSER.equals("Mozilla")) {
+                System.setProperty("webdriver.gecko.driver", "src/test/resources/webdrivers/geckodriver.exe");
+                // ... dentro de getDriver()
+                /**
+                 * FirefoxOptions options = new FirefoxOptions();
+                 * options.addArguments("--window-size=1920,1080")
+                 */
+                driver = new FirefoxDriver();
+                driver.manage().window().maximize();
+            }
 
 
         }
